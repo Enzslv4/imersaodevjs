4c87c5d1e6c1fa9bfa447e95fcc80740486a1cb2
Enzo Silva <enzosilva142002@gmail.com>
2023-11-13 13:03:07 -0300

    primeiro dia acho q ta pronto

---
+++ aula_1/index.js
@@ -0,0 +1,10 @@
+alert('Primeiro site no Alura');
+
+var valorEmDolar = 30
+var cotacaoDoDolar = 5.32
+var valorEmReal = valorEmDolar * cotacaoDoDolar
+valorEmReal = valorEmReal.toFixed([2])
+
+alert(`O valor em Dólar é $ ${valorEmDolar} e o valor em real é R$ ${valorEmReal}`) 
+
+console.log(`O valor em Dólar é $ ${valorEmDolar} e o valor em real é R$ ${valorEmReal}`)
